c3aed11c9d0aa563b8f94977c0a1bdf8081ba2bb
BXn4 <bencefodor@proton.me>
2026-06-12 15:55:20 +0200

    Updated the build information

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 source .env
 
+wails3 task common:update:build-assets
 sed -i "s/^var OA string$/var OA string = \"${OA}\"/" internal/services/api.go
 wails3 package GOOS=windows
 sed -i "s/^var OA string = \"${OA}\"$/var OA string/" internal/services/api.go
